84842d5bea67324125da63712c6eef931d7fdb5e
Luis Eduardo M <luiseduardomc019@gmail.com>
2026-04-07 22:10:37 -0300

    misc

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -3,16 +3,12 @@ import { lazy } from 'react';
 import { createBrowserRouter } from 'react-router';
 import { App } from '@/app/App';
 
-const Home = lazy(() =>
-  import('@/pages/home').then((m) => ({ default: m.Home }))
-);
+const Home = lazy(() => import('@/pages/home').then(m => ({ default: m.Home })));
 
 export const router = createBrowserRouter([
   {
     path: '/',
     Component: App,
-    children: [
-      { index: true, Component: Home },
-    ],
+    children: [{ index: true, Component: Home }],
   },
 ]);
